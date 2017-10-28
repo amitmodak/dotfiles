b9855da81df2495f4728d2c8eec45e50f3d4fcad
Amit Modak <amitmodak@gmail.com>
2017-10-28 11:32:34 +0530

    Add more tools, backup bashrc before overwriting

--- get-my-settings.sh
+++ get-my-settings.sh
@@ -2,14 +2,19 @@
 
 sudo apt-get update
 sudo apt-get install -y \
+   curl \
    git \
    most \
-   pandoc
+   pandoc \
+   vim \
+   wget
 
 curl -L https://github.com/amitmodak/dotfiles/raw/master/screen/.screenrc > ~/.screenrc
-curl -L https://github.com/amitmodak/dotfiles/raw/master/bash/.bashrc >> ~/.bashrc
 curl -L https://github.com/amitmodak/dotfiles/raw/master/vim/.vimrc > ~/.vimrc
 
 curl -L https://raw.githubusercontent.com/git/git/v2.14.3/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
+
+mv ~/.bashrc ~/.bashrc.$(date +%s)
+curl -L https://github.com/amitmodak/dotfiles/raw/master/bash/.bashrc >> ~/.bashrc/
 
 source ~/.bashrc
