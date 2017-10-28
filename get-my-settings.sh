69fd8ddac710542576c6fa09e82ee6c33c1bbef2
Amit Modak <amitmodak@gmail.com>
2017-10-28 11:42:37 +0530

    Fix installation script

--- get-my-settings.sh
+++ get-my-settings.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y \
    git \
    most \
    pandoc \
+   tree \
    vim \
    wget
 
@@ -14,7 +15,7 @@ curl -L https://github.com/amitmodak/dotfiles/raw/master/vim/.vimrc > ~/.vimrc
 
 curl -L https://raw.githubusercontent.com/git/git/v2.14.3/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 
-mv ~/.bashrc ~/.bashrc.$(date +%s)
-curl -L https://github.com/amitmodak/dotfiles/raw/master/bash/.bashrc >> ~/.bashrc/
+cp ~/.bashrc ~/.bashrc.$(date +%s)
+curl -L https://github.com/amitmodak/dotfiles/raw/master/bash/.bashrc >> ~/.bashrc
 
 source ~/.bashrc
